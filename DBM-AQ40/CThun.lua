8cac9c28ff2210b422717eb246528993113d6036
David Groves <github@fibrecat.org>
2020-08-05 18:26:25 +0100

    Update CThun.lua

--- DBM-AQ40/CThun.lua
+++ DBM-AQ40/CThun.lua
@@ -1,17 +1,17 @@
 local mod	= DBM:NewMod("CThun", "DBM-AQ40", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("@file-date-integer@")
+mod:SetRevision("20200730235534")
 mod:SetCreatureID(15589, 15727)
 mod:SetEncounterID(717)
-mod:SetMinSyncRevision(20200804000000)--2020, 8, 04
-
 mod:RegisterCombat("combat")
 mod:SetWipeTime(25)
 
 mod:RegisterEventsInCombat(
+    "SPELL_CAST_START 26134 20604",
+	"SPELL_CAST_SUCCESS 26134 20604",
 	"CHAT_MSG_MONSTER_EMOTE",
-	"UNIT_DIED"
+	"UNIT_DIED"	 
 )
 
 local warnEyeTentacle		= mod:NewAnnounce("WarnEyeTentacle", 2, 126)
@@ -23,6 +23,9 @@ local warnPhase2			= mod:NewPhaseAnnounce(2)
 local specWarnDarkGlare		= mod:NewSpecialWarningDodge(26029, nil, nil, nil, 3, 2)
 local specWarnWeakened		= mod:NewSpecialWarning("SpecWarnWeakened", nil, nil, nil, 2, 2, nil, 28598)
 
+local specWarnEyeBeam		= mod:NewSpecialWarningYou(26134, nil, nil, nil, 1, 2)
+local yellEyeBeam			= mod:NewYell(26134)
+
 local timerDarkGlareCD		= mod:NewNextTimer(86, 26029)
 local timerDarkGlare		= mod:NewBuffActiveTimer(39, 26029)
 local timerEyeTentacle		= mod:NewTimer(45, "TimerEyeTentacle", 126, nil, nil, 1)
@@ -32,6 +35,7 @@ local timerEyeTentacle		= mod:NewTimer(45, "TimerEyeTentacle", 126, nil, nil, 1)
 local timerWeakened			= mod:NewTimer(45, "TimerWeakened", 28598)
 
 mod:AddRangeFrameOption("10")
+mod:AddSetIconOption("SetIconOnEyeBeam", 26134, true, false, {4})
 
 mod.vb.phase = 1
 local firstBossMod = DBM:GetModByName("AQ40Trash")
@@ -54,8 +58,8 @@ function mod:OnCombatEnd(wipe, isSecondRun)
 	end
 	--Only run on second run, to ensure trash mod has had enough time to update requiredBosses
 	if not wipe and isSecondRun and firstBossMod.vb.firstEngageTime and firstBossMod.Options.SpeedClearTimer then
-		if firstBossMod.vb.requiredBosses < 5 then
-			DBM:AddMsg(L.NotValid:format(5 - firstBossMod.vb.requiredBosses .. "/4"))
+		if firstBossMod.vb.requiredBosses < 4 then
+			DBM:AddMsg(L.NotValid:format(4 - firstBossMod.vb.requiredBosses .. "/3"))
 		end
 	end
 end
@@ -88,8 +92,29 @@ function mod:UNIT_DIED(args)
 		self.vb.phase = 2
 		warnPhase2:Show()
 		timerDarkGlareCD:Stop()
-		timerEyeTentacle:Stop()
 		self:UnscheduleMethod("EyeTentacle")
 		self:UnscheduleMethod("DarkGlare")
+	end
+end
+
+do
+	local EyeBeam = DBM:GetSpellInfo(26134)
+	function mod:EyeBeamTarget(targetname, uId)
+		if not targetname then return end
+		if targetname == UnitName("player") then
+			specWarnEyeBeam:Show()
+			specWarnEyeBeam:Play("targetyou")
+			yellEyeBeam:Yell()
+		end
+		if self.Options.SetIconOnEyeBeam then
+			self:SetIcon(targetname, 4)
+		end
+	end
+
+	function mod:SPELL_CAST_START(args)
+		local spellName = args.spellName
+		if spellName == EyeBeam and args:IsSrcTypeHostile() then
+			self:BossTargetScanner(args.sourceGUID, "EyeBeamTarget", 0.05, 16)
+		end
 	end
 end
